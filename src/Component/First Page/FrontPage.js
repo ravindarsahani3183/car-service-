acb3293e800c5a177ab1441b2c29f5ad226e233d
ravindarsahani3183 <ravindarsahani3183@gmail.com>
2024-02-07 21:59:05 +0530

    add image front page

---
+++ src/Component/First Page/FrontPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import service_icon from "../img/service.jpg";
+import Wash_icon from "../img/Wash_car.jpeg";
+import Booking_icon from "../img/Booking_car.jpeg";
+const FrontPage = () => {
+  return (
+    <div>
+        <img src={service_icon} alt=''></img>
+        <img src={Wash_icon} alt=''></img>
+        <img src={Booking_icon} alt=''></img>
+    </div>
+  )
+}
+
+export default FrontPage
