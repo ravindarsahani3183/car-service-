6fc7a3f71d0940087fcfd8b91a7aa373bd13a9ae
ravindarsahani3183 <ravindarsahani3183@gmail.com>
2024-02-11 22:11:13 +0530

    add slider

--- src/Component/First Page/FrontPage.js
+++ src/Component/First Page/FrontPage.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import service_icon from "../img/service.jpg";
 import Wash_icon from "../img/Wash_car.jpeg";
 import Booking_icon from "../img/Booking_car.jpeg";
 const FrontPage = () => {
+  const [showslider, setShowSlider] = useState(true)
   return (
     <div>
         <img src={service_icon} alt=''></img>
         <img src={Booking_icon} alt=''></img>
+        {
+          
+        }
     </div>
   )
 }
