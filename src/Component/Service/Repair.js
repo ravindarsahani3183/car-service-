2025639b1614d617c278ec75729d4657afebc7ae
ravindarsahani3183 <ravindarsahani3183@gmail.com>
2024-03-04 22:25:26 +0530

    complete repair component

--- src/Component/Service/Repair.js
+++ src/Component/Service/Repair.js
@@ -8,29 +8,29 @@ const Repair = () => {
     <div>
         <div>
             <h2 className='repair-title'>Car Repair and Service</h2>
-            <p>Our representatives are professionaly trained and skilled with latest and futursitic techniques to provide a best quality service.</p>
-            <p>At AutoMob-Mechanic, we provide a high class service to the customers for their happy and memorable driving experience.</p>
+            <p className='repair-desc'>Our representatives are professionaly trained and skilled with latest and futursitic techniques to provide a best quality service.</p>
+            <p className='repair-desc'>At AutoMob-Mechanic, we provide a high class service to the customers for their happy and memorable driving experience.</p>
             <div className='repair'>
                 <div className='repair-card'>
-                    <h4>Preventive Maintenance Service</h4>
-                    <img src={wash_icon} className="car-img" alt=''></img>
-                    <p>Periodically check and keep your car running.</p>
-                    <span>Offer: 20%</span>
-                    <button>more detail</button>
+                    <h4 className='repair-card-title'>Preventive Maintenance Service</h4>
+                    <img src={service3_icon} className="repair-img" alt=''></img>
+                    <p className='repair-card-content'>Periodically check and keep your car running.</p>
+                    <p className='repair-card-offer'>Offer: 20%</p>
+                    <button className='repair-card-button'>more details</button>
                 </div>
                 <div className='repair-card'>
-                <h4>Preventive Maintenance Service</h4>
-                    <img src={service3_icon} className="car-img" alt=''></img>
-                    <p>Periodically check and keep your car running.</p>
-                    <span>Offer: 20%</span>
-                    <button>more detail</button>
+                <h4 className='repair-card-title'>Body Repair Service</h4>
+                    <img src={repair_icon} className="repair-img" alt=''></img>
+                    <p className='repair-card-content'>Full chasis body repair provided by the experts.</p>
+                    <p className='repair-card-offer'>Offer: 15%</p>
+                    <button className='repair-card-button'>more details</button>
                 </div>
                 <div className='repair-card'>
-                <h4>Preventive Maintenance Service</h4>
-                    <img src={repair_icon} className="car-img" alt=''></img>
-                    <p>Periodically check and keep your car running.</p>
-                    <span>Offer: 20%</span>
-                    <button>more detail</button>
+                <h4 className='repair-card-title'>Car Care Service</h4>
+                    <img src={wash_icon} className="repair-img" alt=''></img>
+                    <p className='repair-card-content'>Get your car sparkling clean in just minutes.</p>
+                    <p className='repair-card-offer'>Offer: 10%</p>
+                    <button className='repair-card-button'>more details</button>
                 </div>
             </div>
         </div>
