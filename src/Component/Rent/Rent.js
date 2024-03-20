6d58f089613ad104e407993c5b9a4f770745516a
ravindarsahani3183 <ravindarsahani3183@gmail.com>
2024-03-20 19:20:03 +0530

    add componenet

--- src/Component/Rent/Rent.js
+++ src/Component/Rent/Rent.js
@@ -48,9 +48,6 @@ const Rent = () => {
         <div className='rent-card'>
           <img src={nissan} className="rent-img" alt=''></img>
         </div>
-        <div className='rent-card'>
-          <img src={nissan} className="rent-img" alt=''></img>
-        </div>
       </Carousel>
       </div>
     </div>
