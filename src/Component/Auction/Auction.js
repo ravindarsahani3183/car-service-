fc71e438bd9458ec33eab3dfde11da41f3ee2b07
ravindarsahani3183 <ravindarsahani3183@gmail.com>
2024-04-21 22:45:57 +0530

    add contain

--- src/Component/Auction/Auction.js
+++ src/Component/Auction/Auction.js
@@ -4,7 +4,10 @@ const Auction = () => {
   return (
     <div>
       <div className='auction'>Auction</div>
-      
+      <div className='auction'>Auction</div>
+      <div className='auction'>Auction</div>
+      <div className='auction'>Auction</div>
+      <div className='auction'>Auction</div>
     </div>
   )
 }
