77c8672de8da581abef0c1624e64a40a55334491
ravindarsahani3183 <ravindarsahani3183@gmail.com>
2024-04-28 17:21:35 +0530

    add sell component contains

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FrontPage from './Component/First Page/FrontPage';
 import Navbar from './Component/Navbar/Navbar';
 import Rent from './Component/Rent/Rent';
 import Search from './Component/Search/Search';
+import Sell from './Component/Sell/Sell';
 import Repair from './Component/Service/Repair';
 import Service from './Component/Service/Service';
 
@@ -14,6 +15,7 @@ function App() {
       <FrontPage></FrontPage>
       <Search></Search>
       <Service></Service>
+      <Sell></Sell>
       <Repair></Repair>
       <Rent></Rent>
       <Auction></Auction>
---
+++ src/Component/Sell/Sell.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./sell.css";
+const Sell = () => {
+  return (
+    <div className='sell'>
+      <div className='sale'>
+        <div className='sale-intro'>
+          <h1 className='sale-title'>car Sale in 24 hours</h1>
+          <p>Calculate your car's value, then choose how you want to sell</p>
+        </div>
+        <div className='sale-detail'></div>
+      </div>
+      <div>
+        <h2>Popular car body types</h2>
+        <div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sell
